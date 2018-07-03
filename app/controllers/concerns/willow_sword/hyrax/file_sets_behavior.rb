07ca7121a562542d8f58b168ee10b59736880088
Anusha Ranganathan <anusha3@gmail.com>
2018-07-03 11:23:45 +0100

    Controller methods for file create and file metadata update

---
+++ app/controllers/concerns/willow_sword/hyrax/file_sets_behavior.rb
@@ -0,0 +1,37 @@
+module WillowSword
+  module Hyrax
+    module FilesBehavior
+      extend ActiveSupport::Concern
+
+      def find_file_set
+        puts 'In find_file_set'
+        return find_file_set_by_id if params[:id]
+      end
+
+      def find_file_set_by_id
+        puts 'In find_file_set_by_id'
+        FileSet.find(params[:id]) if FileSet.exists?(params[:id])
+      end
+
+      def update_file_set
+        puts 'In update_file_set'
+        raise "File set doesn't exist" unless @file_set
+        @file_set.update!(file_set_update_attributes)
+      end
+
+      def file_set_update_attributes
+        puts 'In file_set_update_attributes'
+        transform_file_set_attributes.except(:id)
+      end
+
+      private
+        def transform_file_set_attributes
+          attributes.slice(*file_set_permitted_attributes)
+        end
+
+        def file_set_permitted_attributes
+          FileSet.properties.keys.map(&:to_sym) + [:id, :edit_users, :edit_groups, :read_groups, :visibility]
+        end
+    end
+  end
+end
--- app/controllers/willow_sword/file_sets_controller.rb
+++ app/controllers/willow_sword/file_sets_controller.rb
@@ -2,11 +2,11 @@ require_dependency "willow_sword/application_controller"
 
 module WillowSword
   class FileSetsController < ApplicationController
-    attr_reader :collection_id, :object_id, :file_set
+    attr_reader :collection_id, :work_id, :file_set, :object
 
     def show
       @collection_id = params[:collection_id]
-      @object_id = params[:object_id]
+      @work_id = params[:work_id]
       @file_set = FileSet.find(params[:id])
       unless @file_set
         message = "Server cannot find file set with id #{params[:id]}"
@@ -14,6 +14,63 @@ module WillowSword
         render '/willow_sword/shared/error.xml.builder', formats: [:xml], status: @error.code
       end
     end
+
+    def create
+      @collection_id = params[:collection_id]
+      @work_id = params[:work_id]
+      if fetch_and_add_file
+        puts "URL #{collection_work_url(params[:collection_id], @object)}"
+        render 'create.xml.builder', formats: [:xml], status: :updated
+      else
+        render '/willow_sword/shared/error.xml.builder', formats: [:xml], status: @error.code
+      end
+    end
+
+    def update
+      @collection_id = params[:collection_id]
+      @work_id = params[:work_id]
+      if fetch_and_add_metadata
+        render status: :updated
+      else
+        render '/willow_sword/shared/error.xml.builder', formats: [:xml], status: @error.code
+      end
+    end
+
+    private
+      def fetch_and_add_file
+        fetch_headers
+        return false unless save_binary_data
+        return false unless validate_binary_data
+        process_file
+        upload_files
+        @klass = Work
+        @object = klass.find(params[:work_id]) if klass.exists?(params[:work_id])
+        if @object
+          update_work
+        end
+        true
+      end
+
+      def fetch_and_add_metadata
+        return false unless save_binary_data
+        return false unless validate_binary_data
+        fetch_data_content_type
+        return false unless process_metadata
+        unless params[:id]
+          message = "Missing identifier: Unable to search for existing file set without the ID"
+          @error = WillowSword::Error.new(message, type = :default)
+          return false
+        end
+        @file_set = find_file_set
+        unless @file_set
+          message = "Missing file set: Unable to search for existing file set with the ID #{params[:id]}"
+          @error = WillowSword::Error.new(message, type = :default)
+          return false
+        end
+        update_file_set
+        true
+      end
+
 
   end
 end
