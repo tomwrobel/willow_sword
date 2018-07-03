09b64fa2fd357d6ac30ee2f1502a8f27b689b7be
Anusha Ranganathan <anusha3@gmail.com>
2018-07-03 00:39:28 +0000

    Extended crosswalk to support translated terms and single valued terms

--- lib/willow_sword/dc_crosswalk.rb
+++ lib/willow_sword/dc_crosswalk.rb
@@ -7,7 +7,7 @@ module WillowSword
       map_xml
     end
     def map_xml
-      return metadata unless File.exist? @src_file
+      return @metadata unless File.exist? @src_file
       f = File.open(@src_file)
       doc = Nokogiri::XML(f)
       # doc = Nokogiri::XML(@xml_metadata)
@@ -21,12 +21,21 @@ module WillowSword
         isVersionOf language license mediator medium modified provenance
         publisher references relation replaces requires rights rightsHolder
         source spatial subject tableOfContents temporal title type valid)
+      translated_terms = {
+          'created' =>'date_created',
+          'rights' => 'rights_statement',
+          'relation' => 'related_url',
+          'type' => 'resource_type'
+      }
+      singular = %w(rights)
       terms.each do |term|
         values = []
         doc.xpath("//#{term}").each do |t|
           values << t.text if t.text.present?
         end
-        @metadata[term.to_sym] = values unless values.blank?
+        key = translated_terms.include?(term) ? translated_terms[term] : term
+        values = values.first if values.present? && singular.include?(term)
+        @metadata[key.to_sym] = values unless values.blank?
       end
       f.close
     end
