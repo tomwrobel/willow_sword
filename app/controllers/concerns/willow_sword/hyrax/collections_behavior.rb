222a8ffce84ac7c0af5ea2a887c53d58fae4b918
Anusha Ranganathan <anusha3@gmail.com>
2018-07-03 03:47:01 +0100

    Prevent errors if collections not found

--- app/controllers/concerns/willow_sword/hyrax/collections_behavior.rb
+++ app/controllers/concerns/willow_sword/hyrax/collections_behavior.rb
@@ -3,7 +3,7 @@ module WillowSword
     module CollectionsBehavior
       extend ActiveSupport::Concern
       def show
-        @collection = Collection.find(params[:id])
+        @collection = Collection.find(params[:id]) if Collection.exists?(params[:id])
         @works = []
         @works = @collection.works if @collection.present?
       end
