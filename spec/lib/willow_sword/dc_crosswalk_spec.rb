27f85ebe80361d4f8cd02bc0bf30cf2106bc888b
Anusha Ranganathan <anusha3@gmail.com>
2018-07-12 16:50:04 +0100

    More tests for dc crosswalk

--- spec/lib/willow_sword/dc_crosswalk_spec.rb
+++ spec/lib/willow_sword/dc_crosswalk_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe WillowSword::DcCrosswalk do
   describe 'DcCrosswalk with no source' do
     before(:each) do
       @src = nil
-      # @src = File.join(ENGINE_RAILS_ROOT, 'spec', 'fixtures', 'dc.xml')
       # @no_src = File.join(ENGINE_RAILS_ROOT, 'spec', 'fixtures', 'dc2.xml')
       @dc = WillowSword::DcCrosswalk.new(@src)
     end
@@ -30,11 +29,63 @@ RSpec.describe WillowSword::DcCrosswalk do
       expect(@dc.metadata).to be_empty
     end
 
-    it 'should return without erroring' do
+    it 'should return empty crosswalk' do
       @dc.map_xml
-      expect(@dc.translated_terms).to be_kind_of(Hash)
+      expect(@dc.metadata).to be_kind_of(Hash)
       expect(@dc.metadata).to be_empty
     end
+  end
 
+  describe 'DcCrosswalk with valid source' do
+    before(:each) do
+      @src = File.join(ENGINE_RAILS_ROOT, 'spec', 'fixtures', 'dc.xml')
+      # @no_src = File.join(ENGINE_RAILS_ROOT, 'spec', 'fixtures', 'dc2.xml')
+      @dc = WillowSword::DcCrosswalk.new(@src)
+    end
+
+    it 'should return a valid crosswalk' do
+      @dc.map_xml
+      expect(@dc.metadata).to be_kind_of(Hash)
+      metadata = {
+        :title=>["Test record"],
+        :creator=>["Anusha"],
+        :subject=>["Sword", "Crosswalk"],
+        :description=>["This is a test dc record to test the dc crosswalk"],
+        :publisher=>["Digital Nest"],
+        :contributor=>["The dublin core generator"],
+        :date_created=>["29/05/2018"],
+        :source=>["http://sword.digitalnest.com"],
+        :language=>["English"],
+        :related_url=>["http://example.com"],
+        :rights_statement=>"CC-0",
+      }
+      expect(@dc.metadata == metadata).to be true
+    end
+  end
+
+  describe 'DcCrosswalk with invalid source' do
+    before(:each) do
+      no_src = File.join(ENGINE_RAILS_ROOT, 'spec', 'fixtures', 'dc2.xml')
+      @dc = WillowSword::DcCrosswalk.new(no_src)
+    end
+
+    it 'should return a empty crosswalk' do
+      @dc.map_xml
+      expect(@dc.metadata).to be_kind_of(Hash)
+      expect(@dc.metadata).to be_empty
+    end
+  end
+
+  describe 'DcCrosswalk with non xml source' do
+    before(:each) do
+      src = File.join(ENGINE_RAILS_ROOT, 'spec', 'fixtures', 'testPackage', 'Swordv2Spec.pdf')
+      @dc = WillowSword::DcCrosswalk.new(src)
+    end
+
+    it 'should return a empty crosswalk' do
+      @dc.map_xml
+      expect(@dc.metadata).to be_kind_of(Hash)
+      expect(@dc.metadata).to be_empty
+    end
   end
 end
