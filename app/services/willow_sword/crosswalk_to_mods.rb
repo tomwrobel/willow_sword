69d591b17180e143d5556591073fa72ef7db4a15
Tom Wrobel <thomas.wrobel@bodleian.ox.ac.uk>
2019-07-31 14:14:04 +0100

    set doi to multivalued

--- app/services/willow_sword/crosswalk_to_mods.rb
+++ app/services/willow_sword/crosswalk_to_mods.rb
@@ -196,9 +196,6 @@ module WillowSword
       child = 'publishers'
       fields.each do |field, tag|
         val = get_grand_child_content(parent, child, field)
-        if val != nil and field == 'identifier_doi'
-          val = val[0]
-        end
         @doc.root << create_node('mods:identifier', val, {'type' => tag}) unless val.blank?
       end
     end
